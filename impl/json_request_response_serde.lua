b905d75a2ab0566783099ecb7671ff4bac015b9d
Arsentii Nerushev <asnerushev@gmail.com>
2019-01-26 00:12:50 +0300

    WIP: json serde.

--- impl/json_request_response_serde.lua
+++ impl/json_request_response_serde.lua
@@ -1,25 +1,17 @@
 package.path = "../?.lua;" .. package.path
 
 local RequestResponseSerde = require("impl.request_response_serde")
-local json = require("utils.json")
+local json_helper = require("impl.json_helper")
 
 local JsonRequestResponseSerde = {}
 setmetatable(JsonRequestResponseSerde, {__index = RequestResponseSerde})
 
 function JsonRequestResponseSerde:deserialize_request (serialized_request)
-  
-  if serialized_request == nil then error("Аргумент #0 'serialized_request' не должен быть nil.") end
-  
-  local deserialized_request = json.decode(serialized_request)
-
-  return deserialized_request.method, deserialized_request.args
+  return json_helper.decode_request(serialized_request)
 end
 
 function JsonRequestResponseSerde:serialize_response (deserialized_response)
-  
-  if deserialized_response == nil then error("Аргумент #0 'deserialized_response' не должен быть nil.") end
-  
-  return json.encode(deserialized_response.proc_result) -- TODO: broken atm. Implement it similar to the protobuf object mapping logic (wrapping the proc_result to a result object).
+  return json_helper.encode_response(deserialized_response)
 end
 
 return JsonRequestResponseSerde
