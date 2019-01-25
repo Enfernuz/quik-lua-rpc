22f8ea67f82df1be9cc248127c3d04672353eba1
Arsentii Nerushev <asnerushev@gmail.com>
2019-01-26 00:27:51 +0300

    Removed a debug file.

--- impl/test.lua
+++
@@ -1,9 +0,0 @@
-package.path = "../?.lua;" .. package.path
-
-local rpc_handler = require("impl.rpc-handler")
-
-local json_handler = rpc_handler:new('json')
-local pb_handler = rpc_handler:new('protobuf')
-
-pcall(function() json_handler.call_procedure('qwerty', {}) end)
-pcall(function() pb_handler.call_procedure(123, {}) end)
